ae662553ac6b509b12b5330e78fd69da257160bf
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-07-21 16:43:19 +0200

    inserimento componente title

---
+++ src/components/Title.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <header>
+       <h1>LA MIA PRIMA APP CON VUE CLI!</h1>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+    name:"Header",
+  }
+}
+</script>
+
+<style>
+#app {
+    background-color: blue;
+    color:white;
+    text-align:center;
+}
+</style>
